62a3f397b09919cfadc170d7f7420ee38d635ea9
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-10-04 08:30:59 +0200

    creating CompileService

--- CompileService.cs
+++ CompileService.cs
@@ -1,11 +1,123 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace CsharpCompiler
 {
     public class CompileService
     {
+        private readonly HttpClient _http;
+        private readonly NavigationManager _uriHelper;
+        public List<string> CompileLog { get; set; }
+        private List<MetadataReference> references { get; set; }
+
+
+        public CompileService(HttpClient http, NavigationManager uriHelper)
+        {
+            _http = http;
+            _uriHelper = uriHelper;
+        }
+
+        public async Task Init()
+        {
+            if (references == null)
+            {
+                references = new List<MetadataReference>();
+                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    if (assembly.IsDynamic)
+                    {
+                        continue;
+                    }
+                    var name = assembly.GetName().Name + ".dll";
+                    Console.WriteLine(name);
+
+                    string path = _uriHelper.BaseUri + "/_framework/_bin/" + name;
+                    Console.WriteLine(path);
+                    try
+                    {
+                        var s = await this._http.GetStreamAsync(path);
+                        references.Add(
+                            MetadataReference.CreateFromStream(s));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        public async Task<Assembly> Compile(string code)
+        {
+            await Init();
+
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code, new CSharpParseOptions(LanguageVersion.Preview));
+            foreach (var diagnostic in syntaxTree.GetDiagnostics())
+            {
+                CompileLog.Add(diagnostic.ToString());
+            }
+
+            if (syntaxTree.GetDiagnostics().Any(i => i.Severity == DiagnosticSeverity.Error))
+            {
+                CompileLog.Add("Parse SyntaxTree Error!");
+                return null;
+            }
+
+            CompileLog.Add("Parse SyntaxTree Success");
+
+            CSharpCompilation compilation = CSharpCompilation.Create("CompileBlazorInBlazor.Demo", new[] { syntaxTree },
+                references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(stream);
+
+                foreach (var diagnostic in result.Diagnostics)
+                {
+                    CompileLog.Add(diagnostic.ToString());
+                }
+
+                if (!result.Success)
+                {
+                    CompileLog.Add("Compilation error");
+                    return null;
+                }
+
+                CompileLog.Add("Compilation success!");
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                //                var context = new CollectibleAssemblyLoadContext();
+                Assembly assemby = AppDomain.CurrentDomain.Load(stream.ToArray());
+                return assemby;
+            }
+
+            return null;
+        }
+
+        public async Task<string> CompileAndRun(string code)
+        {
+            await Init();
+
+            var assemby = await this.Compile(code);
+            if (assemby != null)
+            {
+                var type = assemby.GetExportedTypes().FirstOrDefault();
+                var methodInfo = type.GetMethod("Run");
+                var instance = Activator.CreateInstance(type);
+                return (string)methodInfo.Invoke(instance, new object[] { "my UserName", 12 });
+            }
+
+            return null;
+        }
     }
 }
